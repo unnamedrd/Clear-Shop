4b0b15bf849bd1ce85970e341eda61031a4d209a
najwanan <theunnamedrd@gmail.com>
2022-09-25 19:27:35 -0700

    adjusting routes to new landing page

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
-const landingController = require("../controllers/posts")
+const landingController = require("../controllers/landing")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now

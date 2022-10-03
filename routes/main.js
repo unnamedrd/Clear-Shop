c4df3ae880c9586740a206c943131570739e043b
najwanan <theunnamedrd@gmail.com>
2022-10-02 19:15:20 -0700

    fixing routers and controllers to move login to home page

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
-const formController = require("../controllers/form");
+const loginController = require("../controllers/login");
 const postsController = require("../controllers/posts");
 const landingController = require("../controllers/landing")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
-router.get("/", landingController.getLanding);
-router.get("/form", ensureAuth, formController.getIndex)
+
+router.get("/", loginController.getIndex)
+router.get("/landing", ensureAuth, landingController.getLanding);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);

c3c3ab0a7af1d66970e5caa6959fd6ad2a09e7e0
najwanan <theunnamedrd@gmail.com>
2022-10-11 12:16:19 -0700

    formatting username and troubleshooting delete button function

---
+++ public/js/post.js
@@ -0,0 +1,12 @@
+const deleteBtn = document.querySelectorAll(".delBtn");
+
+Array.from(deleteBtn).forEach((el) => {
+  el.addEventListener("click", deleteReview);
+});
+
+
+function deleteReview() {
+    const reviewId = this.parentNode.dataset.id
+
+    console.log("The delete button was clicked for this")
+}

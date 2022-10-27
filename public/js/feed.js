d0c0627a63749de9ebf50513117d9e8246c8279d
najwanan <theunnamedrd@gmail.com>
2022-10-26 20:37:26 -0700

    testing event listener o edit buttons in feed

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,18 +1,41 @@
-const deleteBtn = document.querySelectorAll(".del")
-const editBtn = document.querySelectorAll(".m-3 btn btn-primary edit");
+const deleteBtn = document.querySelectorAll("#del")
+const editBtn = document.querySelectorAll("#edit");
+const testBtn = document.querySelector("#test")
 
-Array.from(deleteBtn).forEach((el) => {
-    el.addEventListener('click', deleteItem)
+//document.querySelector('#Payout').style.display = "none"
+
+
+testBtn.addEventListener("click", function (e) {
+  console.log("hey that is a button!")
 })
 
-Array.from(editBtn).forEach((el) => {
-  el.addEventListener('click', editItem)
-})
+/*Array.from(deleteBtn).forEach((el) => {
+  el.addEventListener('click', function (e) {
+    const deleteCard = this.parentNode.dataset.id
+    
+    try {
+      const response = await fetch(`posts/deletePost`, {
+        method: 'delete', 
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+          'todoIdFromJSFile': ingredientId
+        })
+      })
+      const data = await response.json()
+      console.log(data)
+      location.reload()
+    } catch (err) {
+      console.log(err)
+    }
+    })
+})*/
 
-function editItem() {
-  console.log("you clicked the edit button");
-}
-  
+Array.from(editBtn).forEach((button) => {
+  button.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("you clicked the edit button");
+  })
+})
 
 
 async function deleteItem() {

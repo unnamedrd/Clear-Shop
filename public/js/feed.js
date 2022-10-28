dfcf903e6c5023e301ab6c48f59af2e3e6fb1f27
najwanan <theunnamedrd@gmail.com>
2022-10-27 23:37:20 -0700

    deleting extraneous code

--- public/js/feed.js
+++ public/js/feed.js
@@ -2,47 +2,6 @@ const deleteBtn = document.querySelectorAll("#del")
 const editBtn = document.querySelectorAll("#edit");
 const testBtn = document.querySelector("#test").style.display = "none"
 
-//testBtn.addEventListener("click", function (e) {
- // console.log("hey that is a button!")
-//})
 
-/*Array.from(deleteBtn).forEach((el) => {
-  el.addEventListener('click', function (e) {
-    const deleteCard = this.parentNode.dataset.id
-    
-    try {
-      const response = await fetch(`posts/deletePost`, {
-        method: 'delete', 
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify({
-          'todoIdFromJSFile': ingredientId
-        })
-      })
-      const data = await response.json()
-      console.log(data)
-      location.reload()
-    } catch (err) {
-      console.log(err)
-    }
-    })
-})
 
-async function deleteItem() {
-  console.log("you clicked something");
-  const postId = this.parentNode.dataset.id
-  try {
-    const response = await fetch('posts/deletePost', {
-      method: 'delete', 
-      headers: { 'Content-type': 'application/json' }, 
-      body: JSON.stringify({
-        'id': postId
-      })
-    })
-    const data = await response.json()
-    console.log(data)
-    location.reload()
-  } catch (err) {
-    console.log(err)
-  }
- 
-}*/
+

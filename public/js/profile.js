43b403092635c8deb0865d720db26582b16774ee
najwanan <theunnamedrd@gmail.com>
2022-11-17 23:57:17 -0800

    adding button functionality to toggle business form

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 //const ratingStars = [...document.getElementById("rating")];
-const addReviewBtn = document.querySelector("#addReview")
+const addBusinessBtn = document.querySelector("#getForm");
 
 const businessForm = document.querySelector("#BusinessProfileFormCard");
 
 
 //form.style.display = "none"
 
-addReviewBtn.addEventListener("click", addReview)
-function addReview() {
-
+addBusinessBtn.addEventListener("click", addBusiness)
+function addBusiness() {
+   
   if (businessForm.style.display == "none") {
   
      businessForm.style.display = "block";
